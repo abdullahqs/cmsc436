fd57ef3e6279c5100b4ee1971134154b32ff5ef2
Ramsey Khadder <ramseykhadder@hotmail.com>
2017-02-01 13:44:52 -0800

    added tap test area

--- app/src/main/java/com/ms/tests/TapTestActivity.java
+++ app/src/main/java/com/ms/tests/TapTestActivity.java
@@ -2,6 +2,8 @@ package com.ms.tests;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TapTestActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TapTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap_test);
+
+        ImageView testArea = (ImageView) findViewById(R.id.tap_test_area);
+        testArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
