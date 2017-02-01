ca7e0771537223dd5b3def149b327191490ff878
Carlos Gonzales <cnexus0@gmail.com>
2017-02-01 15:28:53 -0500

    Initial code dump, main activity, tap activity, and tap results activity added

---
+++ app/src/main/java/com/ms/tests/TapTestResultActivity.java
@@ -0,0 +1,13 @@
+package com.ms.tests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TapTestResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap_test_result);
+    }
+}
---
+++ app/src/main/java/com/ms/tests/TestSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.ms.tests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_selection);
+    }
+}
