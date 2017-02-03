cf55da84e13da774167600fcd8009679c5aeadca
Ramsey Khadder <ramseykhadder@hotmail.com>
2017-02-03 13:56:36 -0800

    added intent logic

--- app/src/main/java/com/ms/tests/TapTestActivity.java
+++ app/src/main/java/com/ms/tests/TapTestActivity.java
@@ -1,5 +1,6 @@
 package com.ms.tests;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +10,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TapTestActivity extends AppCompatActivity {
+    private TapTestResults _testResults;
+    private int _testRound;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap_test);
 
+        Intent i = getIntent();
+        _testResults = (TapTestResults)i.getParcelableExtra("TEST_RESULTS");
+        _testRound = i.getIntExtra("TEST_ROUND", 1);
+
+        initCountdown();
+    }
+
+    void initCountdown() {
         // Handles count down at beginning
         final ImageView countDownBG = (ImageView) findViewById(R.id.tap_test_bg);
         final TextView countDownText = (TextView) findViewById(R.id.tap_test_title);
@@ -26,16 +38,39 @@ public class TapTestActivity extends AppCompatActivity {
             public void onFinish() {
                 ((ViewGroup)countDownBG.getParent()).removeView(countDownBG);
                 ((ViewGroup)countDownText.getParent()).removeView(countDownText);
-                // Start test
+                startTest();
             }
         }.start();
+    }
 
+    void startTest() {
         ImageView testArea = (ImageView) findViewById(R.id.tap_test_area);
         testArea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                if (true) {
+                    finishTest();
+                }
             }
         });
+    }
+
+    void finishTest() {
+        int results = 0;
+        _testResults.TestResults[_testRound - 1] = results;
+
+        _testRound += 1;
+
+        Intent i;
+        if (_testRound <= _testResults.NumTests) {
+            i = new Intent(getBaseContext(), TapTestActivity.class);
+            i.putExtra("TEST_ROUND", _testRound);
+        } else {
+            i = new Intent(getBaseContext(), TapTestResults.class);
+        }
+        i.putExtra("TEST_RESULTS", _testResults);
+
+        startActivity(i);
     }
 }
