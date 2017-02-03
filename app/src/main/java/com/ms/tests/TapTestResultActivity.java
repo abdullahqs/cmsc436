8c9e4ab04617c4f76a8c560266d389d6bc410fb6
Ramsey Khadder <ramseykhadder@hotmail.com>
2017-02-03 14:03:07 -0800

    added intent logic and added some fake data

--- app/src/main/java/com/ms/tests/TapTestResultActivity.java
+++ app/src/main/java/com/ms/tests/TapTestResultActivity.java
@@ -1,5 +1,6 @@
 package com.ms.tests;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class TapTestResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap_test_result);
+
+        Intent i = getIntent();
+        // PRODUCTION - TapTestResults results = (TapTestResults) i.getParcelableExtra("TEST_RESULTS");
+        // TESTING
+        TapTestResults results = new TapTestResults(true, 4);
+        results.TestResults[0] = 8;
+        results.TestResults[1] = 4;
+        results.TestResults[2] = 12;
+        results.TestResults[3] = 6;
     }
 }
