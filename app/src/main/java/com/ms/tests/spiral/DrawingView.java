b7b990c0471a316646ddd749e33cca221442130c
Ramsey Khadder <ramseykhadder@hotmail.com>
2017-02-13 13:28:57 -0800

    saves spiral image

--- app/src/main/java/com/ms/tests/spiral/DrawingView.java
+++ app/src/main/java/com/ms/tests/spiral/DrawingView.java
@@ -1,6 +1,7 @@
 package com.ms.tests.spiral;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -13,6 +14,10 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.ms.tests.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Ramse on 2/13/2017.
@@ -45,6 +50,8 @@ public class DrawingView extends View {
     }
 
     void Init(Context c) {
+        setDrawingCacheEnabled(true);
+
         mPaint = new Paint();
         mPaint.setColor(Color.RED);
         mPaint.setStyle(Paint.Style.STROKE);
@@ -76,6 +83,7 @@ public class DrawingView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.drawColor(Color.WHITE);
         canvas.drawBitmap(spiral, spiralX, spiralY, mBitmapPaint);
         canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
         canvas.drawPath( mPath,  mPaint);
@@ -129,6 +137,32 @@ public class DrawingView extends View {
                 touch_up();
                 invalidate();
                 break;
+        }
+        return true;
+    }
+
+    public boolean SaveImage(String fileName) {
+
+        ContextWrapper cw = new ContextWrapper(context);
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("tests/spiral", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,fileName);
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            getDrawingCache().compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return true;
     }
--- app/src/main/java/com/ms/tests/spiral/SpiralTestActivity.java
+++ app/src/main/java/com/ms/tests/spiral/SpiralTestActivity.java
@@ -1,23 +1,35 @@
 package com.ms.tests.spiral;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import com.ms.tests.R;
+import com.ms.tests.TestSelectionActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Locale;
 
 public class SpiralTestActivity extends AppCompatActivity {
 
-    DrawingView dv ;
-
+    DrawingView dv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +37,40 @@ public class SpiralTestActivity extends AppCompatActivity {
         setContentView(dv);*/
 
         setContentView(R.layout.activity_spiral_test);
+        dv = (DrawingView) findViewById(R.id.spiralTest);
     }
 
+    public void onClick(View v) {
 
+        File file=new File(Environment.getExternalStorageDirectory()+"/ms/spiral");
+        if(!file.isDirectory()){
+            file.mkdir();
+        }
+
+        file=new File(Environment.getExternalStorageDirectory()+"/ms/spiral",System.currentTimeMillis()+".jpg");
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            dv.getDrawingCache().compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+
+
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "Spiral Test");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Spiral Test for MS");
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis ());
+            values.put(MediaStore.Images.ImageColumns.BUCKET_ID, file.toString().toLowerCase(Locale.US).hashCode());
+            values.put(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME, file.getName().toLowerCase(Locale.US));
+            values.put("_data", file.getAbsolutePath());
+
+            ContentResolver cr = getContentResolver();
+            cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Intent i = new Intent(SpiralTestActivity.this, TestSelectionActivity.class);
+        startActivity(i);
+    }
 }
