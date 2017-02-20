42a3c9d38b74de4e74a38e704b5f29cfc5af4bfa
Ramsey Khadder <ramseykhadder@hotmail.com>
2017-02-20 14:25:26 -0800

    added level test results page

---
+++ app/src/main/java/com/ms/tests/level/LevelTestResultsActivity.java
@@ -0,0 +1,48 @@
+package com.ms.tests.level;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.ms.tests.R;
+import com.ms.tests.spiral.SpiralTestActivity;
+
+public class LevelTestResultsActivity extends AppCompatActivity {
+    private Button mButton;
+    private ImageView mResultView;
+    private Uri mImageUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_test_results);
+
+        mButton = (Button) findViewById(R.id.level_results_share_button);
+        mResultView = (ImageView) findViewById(R.id.level_results_result_preview);
+
+        Intent i = getIntent();
+
+        String resultImage = i.getStringExtra(SpiralTestActivity.RESULT_IMAGE_URI);
+        if(resultImage != null) {
+            mImageUri = Uri.parse(resultImage);
+            mResultView.setImageURI(mImageUri);
+        }
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mImageUri != null){
+                    Intent share = new Intent(Intent.ACTION_SEND);
+                    share.putExtra(Intent.EXTRA_STREAM, mImageUri);
+                    share.setType("image/jpeg");
+                    startActivity(Intent.createChooser(share, "Share Results"));
+                }
+            }
+        });
+    }
+}
