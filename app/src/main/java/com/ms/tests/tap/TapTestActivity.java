8809de134186bec63271fbcd86e1416c0b5c3012
Andrew Dicken <Andrew Dicken>
2017-02-20 19:11:19 -0500

    Balloon for tap test

--- app/src/main/java/com/ms/tests/tap/TapTestActivity.java
+++ app/src/main/java/com/ms/tests/tap/TapTestActivity.java
@@ -1,5 +1,7 @@
 package com.ms.tests.tap;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -8,7 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -47,17 +52,25 @@ public class TapTestActivity extends AppCompatActivity {
 
         _tapView = (RelativeLayout) findViewById(R.id.tap_test_view);
 
-        final Button testArea = (Button) findViewById(R.id.tap_test_area);
-        testArea.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    _tapsCount++;
-                    testArea.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.colorPrimaryDark));
-                } else if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                    testArea.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.colorAccent));
-                }
-                return false;
+
+        final Button button  = (Button) findViewById(R.id.tap_test_area);
+        final ImageView image = (ImageView) findViewById(R.id.tap_image);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                _tapsCount++;
+
+                image.setScaleX(image.getScaleX() * 1.1f < 1 ? image.getScaleX() * 1.1f : 1);
+                image.setScaleY(image.getScaleY() * 1.1f < 1 ? image.getScaleY() * 1.1f : 1);
+
+                ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(image, "scaleX", 0.25f);
+                ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(image, "scaleY", 0.25f);
+
+                scaleDownX.setDuration(1000);
+                scaleDownY.setDuration(1000);
+
+                AnimatorSet scaleDown = new AnimatorSet();
+                scaleDown.play(scaleDownX).with(scaleDownY);
+                scaleDown.start();
             }
         });
 
@@ -74,7 +87,7 @@ public class TapTestActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                ((ViewGroup)countDownBG.getParent()).removeView(countDownBG);
+                //((ViewGroup)countDownBG.getParent()).removeView(countDownBG);
                 ((ViewGroup)countDownText.getParent()).removeView(countDownText);
                 startTest();
             }
