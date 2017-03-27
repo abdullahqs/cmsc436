e41b43f730b236a2eb3b3f01f4dcbe915028c3a8
alee112 <alee112@terpmail.umd.edu>
2017-03-27 15:35:34 -0400

    adjusting sensitivity for sensor

--- app/src/main/java/com/ms/tests/rotation/RotateTestActivity.java
+++ app/src/main/java/com/ms/tests/rotation/RotateTestActivity.java
@@ -117,60 +117,77 @@ public class RotateTestActivity extends AppCompatActivity implements SensorEvent
         return (int) (Math.abs(degrees) < MIN_THRESHOLD ? 0 : degrees);
     }
 
+    private boolean confirmStart(int pitchDeg, int rollDeg){
+        return pitchDeg <= startend[1];
+    }
+
+    private boolean confirmEnd(int pitchDeg, int rollDeg){
+        return pitchDeg <= startend[3];
+    }
+
+    private boolean confirmMiddle(int pitchDeg, int rollDeg){
+        return pitchDeg >= 80;
+    }
+
+
 
     public void onSensorChanged(SensorEvent event) {
         if (event.values == null) {
             return;
         }
-
-        int sensorType = event.sensor.getType();
-        switch (sensorType) {
-            case Sensor.TYPE_ACCELEROMETER:
-                mGravity = event.values;
-                break;
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                mGeomagnetic = event.values;
-                break;
-            default:
-                Log.w(TAG, "Unknown sensor type " + sensorType);
-                return;
-        }
-
-        if (mGravity == null) {
-            Log.w(TAG, "mGravity is null");
-            return;
-        }
-        if (mGeomagnetic == null) {
-            Log.w(TAG, "mGeomagnetic is null");
-            return;
-        }
-
-        float rot[] = new float[9];
-        if (!SensorManager.getRotationMatrix(rot, null, mGravity, mGeomagnetic)) {
-            Log.w(TAG, "getRotationMatrix() failed");
-            return;
-        }
-
-        float orientation[] = new float[9];
-        SensorManager.getOrientation(rot, orientation);
-
-        // Orientation contains: azimuth, pitch and roll - we'll use roll
-        float pitch = orientation[1];
-        float roll = orientation[2];
-        int rollDeg = scaleAngle(roll);
-        int pitchDeg = -1* scaleAngle(pitch);
-
-        Log.v(TAG, String.format("%d, %d", rollDeg, pitchDeg));
         if(counter <= 9) {
-            if (!listened && rollDeg <= startend[1] && pitchDeg <= startend[0] && process == 0) {
+            int sensorType = event.sensor.getType();
+            switch (sensorType) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    mGravity = event.values;
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    mGeomagnetic = event.values;
+                    break;
+                default:
+                    Log.w(TAG, "Unknown sensor type " + sensorType);
+                    return;
+            }
+
+            if (mGravity == null) {
+                Log.w(TAG, "mGravity is null");
+                return;
+            }
+            if (mGeomagnetic == null) {
+                Log.w(TAG, "mGeomagnetic is null");
+                return;
+            }
+
+            float rot[] = new float[9];
+            if (!SensorManager.getRotationMatrix(rot, null, mGravity, mGeomagnetic)) {
+                Log.w(TAG, "getRotationMatrix() failed");
+                return;
+            }
+
+            float orientation[] = new float[9];
+            SensorManager.getOrientation(rot, orientation);
+
+            // Orientation contains: azimuth, pitch and roll - we'll use roll
+            float pitch = orientation[1];
+            float roll = orientation[2];
+            int rollDeg = scaleAngle(roll);
+            int pitchDeg = -1* scaleAngle(pitch);
+
+            Log.v(TAG, String.format("%d, %d", rollDeg, pitchDeg));
+
+            if (!listened && confirmStart(pitchDeg,rollDeg) && process == 0) {
                 listened = true;
                 process += 1;
                 double temp = System.currentTimeMillis() / 1000;
                 temptime = (int) Math.floor(temp);
-            } else if (listened && rollDeg >= startend[3] && pitchDeg >= startend[2] && process == 1) {
+            } else if(listened && confirmMiddle(pitchDeg,rollDeg) && process == 1){
+                process += 1;
+            } else if (listened && confirmEnd(pitchDeg,rollDeg) && process == 2) {
                 listened = false;
                 process += 1;
-            } else if (!listened && rollDeg <= startend[1] && pitchDeg <= startend[0] && process == 2) {
+            } else if(!listened && confirmMiddle(pitchDeg,rollDeg) && process == 3){
+                process += 1;
+            } else if (!listened && confirmStart(pitchDeg,rollDeg) && process == 4) {
                 double end = System.currentTimeMillis() / 1000;
                 int endsec = (int) Math.floor(end);
                 temptime = endsec - temptime;
@@ -182,6 +199,7 @@ public class RotateTestActivity extends AppCompatActivity implements SensorEvent
                 display.setText(out);
             }
         } else {
+            mStart.setText("See Results");
             return;
         }
 
