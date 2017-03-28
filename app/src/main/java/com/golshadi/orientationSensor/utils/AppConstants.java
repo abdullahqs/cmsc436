9a363952fdf67c914281e5f1ac4c41073d9e40b3
alee112 <alee112@terpmail.umd.edu>
2017-03-27 21:53:06 -0400

    deleted golshadi library

--- app/src/main/java/com/golshadi/orientationSensor/utils/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.golshadi.orientationSensor.utils;
-
-public class AppConstants {
-
-	public final static String AZIMTUH 	= "azimuth";
-	public final static String PITCH 	= "pitch";
-	public final static String ROLL		= "roll";
-}
