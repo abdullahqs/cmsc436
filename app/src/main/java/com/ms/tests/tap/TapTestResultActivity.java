682db0df086162169d1402ac1cb5a7843021893a
bhardwajaakarshan <bhardwaj.aakarshan@gmail.com>
2017-04-08 14:30:46 -0400

    Added Sheets Functionality

--- app/src/main/java/com/ms/tests/tap/TapTestResultActivity.java
+++ app/src/main/java/com/ms/tests/tap/TapTestResultActivity.java
@@ -1,8 +1,11 @@
 package com.ms.tests.tap;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +13,54 @@ import android.widget.TextView;
 import com.ms.tests.R;
 import com.ms.tests.TestSelectionActivity;
 
-public class TapTestResultActivity extends AppCompatActivity {
+import cmsc436.tharri16.googlesheetshelper.CMSC436Sheet;
+
+public class TapTestResultActivity extends AppCompatActivity implements CMSC436Sheet.Host {
+
+    private static final int LIB_ACCOUNT_NAME_REQUEST_CODE = 1001;
+    private static final int LIB_AUTHORIZATION_REQUEST_CODE = 1002;
+    private static final int LIB_PERMISSION_REQUEST_CODE = 1003;
+    private static final int LIB_PLAY_SERVICES_REQUEST_CODE = 1004;
+
+    private CMSC436Sheet sheet;
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        sheet.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        sheet.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public int getRequestCode(CMSC436Sheet.Action action) {
+        switch (action) {
+            case REQUEST_ACCOUNT_NAME:
+                return LIB_ACCOUNT_NAME_REQUEST_CODE;
+            case REQUEST_AUTHORIZATION:
+                return LIB_AUTHORIZATION_REQUEST_CODE;
+            case REQUEST_PERMISSIONS:
+                return LIB_PERMISSION_REQUEST_CODE;
+            case REQUEST_PLAY_SERVICES:
+                return LIB_PLAY_SERVICES_REQUEST_CODE;
+            default:
+                return -1; // boo java doesn't know we exhausted the enum
+        }
+    }
+
+    public Activity getActivity() {
+        return this;
+    }
+
+    public void notifyFinished(Exception e) {
+        if (e != null) {
+            throw new RuntimeException(e); // just to see the exception easily in logcat
+        }
+
+        Log.i(getClass().getSimpleName(), "Done");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +111,9 @@ public class TapTestResultActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        sheet = new CMSC436Sheet(this, getString(R.string.app_name), "1ooKJktuWc0N9SFUkcI8GlgkoRQLGP6mqOwt2TKmOIDo");
+        sheet.writeData(CMSC436Sheet.TestType.LH_TAP, "ayylamo", CMSC436Sheet.unixToSheetsEpoch(System.currentTimeMillis()));
+        sheet.writeData(CMSC436Sheet.TestType.LH_TAP, "ayylamo", 1.23f);
     }
 }
