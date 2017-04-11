06fb7042d605df9cc08134b208b96235ffade458
cnexus <cnexus0@gmail.com>
2017-04-11 05:00:11 -0400

    Fix stuff

--- app/src/main/java/com/ms/tests/sway/SwayTiltView.java
+++ app/src/main/java/com/ms/tests/sway/SwayTiltView.java
@@ -132,8 +132,8 @@ public class SwayTiltView extends View {
 
             bestScore += 1;
 
-            float dist = (float)Math.sqrt(Math.pow(mBubblePoint.x, 2) + Math.pow(mBubblePoint.y, 2));
-            score += 1 - Math.min(dist / mCanvas.getWidth(), 1);
+            float dist = (float)Math.sqrt(Math.pow(mBubblePoint.x - mCanvas.getWidth()/2, 2) + Math.pow(mBubblePoint.y - mCanvas.getHeight()/2, 2));
+            score += 1 - Math.min(dist / (mCanvas.getWidth()/2), 1);
 
 
             if (Math.abs(oldX - mBubblePoint.x) < 5 || Math.abs(oldY - mBubblePoint.y) < 5) {
